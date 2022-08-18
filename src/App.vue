370e2eaa71d1b3435f3f4975097709fdc8dce146
MustafaMwafy <mwafy092@gmail.com>
2022-08-18 14:56:15 +0200

    connect countries api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,73 @@
-<script setup></script>
-
 <template>
-    <div>Nice</div>
-</template>
+    <div class="container-fluid">
+        <div class="row data__container">
+            <div class="col-4 form__container">
+                <h1>دليل الهيئة</h1>
+                <label>
+                    الدول
+                    <select v-model="selectedCountry">
+                        <option value="" selected disabled hidden>
+                            اختار الدوله
+                        </option>
+                        <option v-for="country in countries" :key="country">
+                            {{ country }}
+                        </option>
+                    </select>
+                </label>
 
-<style scoped></style>
+                <label>
+                    المدن
+                    <select>
+                        <option value="" selected disabled hidden>
+                            اختار المدينه
+                        </option>
+                        <option v-for="state in getCountryStates" :key="state">
+                            {{ state }}
+                        </option>
+                    </select>
+                </label>
+                <button @click="fetchCountryList()">
+                    اعرض النتائج
+                    <img src="./assets/arrow.svg" />
+                </button>
+            </div>
+            <div class="col-8">Map</div>
+        </div>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            countries: [],
+            selectedCountry: '',
+            countryStates: {},
+        };
+    },
+    methods: {
+        fetchCountryList() {
+            axios(
+                'https://raw.githubusercontent.com/russ666/all-countries-and-cities-json/master/countries.min.json'
+            ).then((data) => {
+                this.countries = Object.keys(data.data);
+                this.countryStates = data.data;
+            });
+        },
+    },
+    mounted() {
+        this.fetchCountryList();
+    },
+    computed: {
+        getCountryStates() {
+            return this.countryStates[this.selectedCountry];
+        },
+    },
+};
+</script>
+<style scoped lang="scss">
+@import './styles/main.scss';
+@import './styles/form.scss';
+</style>
