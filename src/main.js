117e36ef711b0e1fac00ba487a94ee4836d83bda
MustafaMwafy <mwafy092@gmail.com>
2022-08-18 13:38:09 +0200

    set base code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
-import './assets/main.css'
-
-createApp(App).mount('#app')
+createApp(App).mount('#app');
